97c8ca8e96728f7551eaf5856e44f6dacc6e1fb6
AndyFaneva <fanevahaintsoa@gmail.com>
2024-09-19 08:11:09 +0300

    Première version sur github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterLinkActive} from '@angular/router';
 
 
 @Component({
@@ -10,7 +11,8 @@ import { HomeComponent } from './home/home.component';
   standalone: true,
   imports: [RouterOutlet,
     RouterLink,
-    HomeComponent
+    HomeComponent,
+    RouterLinkActive
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
